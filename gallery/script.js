583b98a9ecc06297313755fdb6d9e7af547ae138
honeyjive <son.hyehyun@gmail.com>
2014-10-08 20:38:51 -0500

    adding alertly

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,17 @@
 // Add your code here
+
+
+//attach a click listener on each button
+// $('#monkey').on('click', function() {
+// 	//grab and hide all the monkeys
+// 		//check if monkeys are hidden
+// 			//if so, show the monkeys
+// 	$('.monkey').hide();
+// });
+
+$('.filter-button').on('click', function (){
+	$('.' + this.id).toggle();
+	$('#' + this.id).toggleClass('active');
+});
+
+// alertify
